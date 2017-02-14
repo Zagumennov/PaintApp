5f4f7c58ccdcad086397edd30fe5bfc2ec576d34
dzagu <dzagu@DESKTOP-2SM9GMN>
2017-02-14 20:52:09 +0700

    Добавьте файлы проекта.

---
+++ PaintApp/MainForm.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace PaintApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.TabFile = new MetroFramework.Controls.MetroTabPage();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroButtonLoadFile = new MetroFramework.Controls.MetroButton();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.tabInput = new MetroFramework.Controls.MetroTabPage();
+            this.metroButtonLine = new MetroFramework.Controls.MetroButton();
+            this.metroButtonRect = new MetroFramework.Controls.MetroButton();
+            this.openGLControlView = new SharpGL.OpenGLControl();
+            this.TabSettings = new MetroFramework.Controls.MetroTabPage();
+            this.tabHelp = new MetroFramework.Controls.MetroTabPage();
+            this.metroTabControl1.SuspendLayout();
+            this.TabFile.SuspendLayout();
+            this.tabInput.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControlView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.TabFile);
+            this.metroTabControl1.Controls.Add(this.tabInput);
+            this.metroTabControl1.Controls.Add(this.TabSettings);
+            this.metroTabControl1.Controls.Add(this.tabHelp);
+            this.metroTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.metroTabControl1.Location = new System.Drawing.Point(20, 60);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 1;
+            this.metroTabControl1.Size = new System.Drawing.Size(1093, 704);
+            this.metroTabControl1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroTabControl1.TabIndex = 10;
+            this.metroTabControl1.UseSelectable = true;
+            // 
+            // TabFile
+            // 
+            this.TabFile.Controls.Add(this.metroLabel4);
+            this.TabFile.Controls.Add(this.metroButtonLoadFile);
+            this.TabFile.Controls.Add(this.metroButton1);
+            this.TabFile.HorizontalScrollbar = true;
+            this.TabFile.HorizontalScrollbarBarColor = true;
+            this.TabFile.HorizontalScrollbarHighlightOnWheel = false;
+            this.TabFile.HorizontalScrollbarSize = 10;
+            this.TabFile.Location = new System.Drawing.Point(4, 38);
+            this.TabFile.Name = "TabFile";
+            this.TabFile.Padding = new System.Windows.Forms.Padding(25);
+            this.TabFile.Size = new System.Drawing.Size(1085, 662);
+            this.TabFile.TabIndex = 2;
+            this.TabFile.Text = "Файл";
+            this.TabFile.VerticalScrollbar = true;
+            this.TabFile.VerticalScrollbarBarColor = true;
+            this.TabFile.VerticalScrollbarHighlightOnWheel = false;
+            this.TabFile.VerticalScrollbarSize = 10;
+            this.TabFile.Visible = false;
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(28, 25);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(189, 19);
+            this.metroLabel4.TabIndex = 8;
+            this.metroLabel4.Text = "Выберите загружаемый файл";
+            // 
+            // metroButtonLoadFile
+            // 
+            this.metroButtonLoadFile.Location = new System.Drawing.Point(540, 74);
+            this.metroButtonLoadFile.Name = "metroButtonLoadFile";
+            this.metroButtonLoadFile.Size = new System.Drawing.Size(104, 23);
+            this.metroButtonLoadFile.TabIndex = 5;
+            this.metroButtonLoadFile.Text = "Загрузить файл";
+            this.metroButtonLoadFile.UseSelectable = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(28, 74);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(104, 23);
+            this.metroButton1.TabIndex = 4;
+            this.metroButton1.Text = "Выбрать файл";
+            this.metroButton1.UseSelectable = true;
+            // 
+            // tabInput
+            // 
+            this.tabInput.Controls.Add(this.metroButtonLine);
+            this.tabInput.Controls.Add(this.metroButtonRect);
+            this.tabInput.Controls.Add(this.openGLControlView);
+            this.tabInput.HorizontalScrollbarBarColor = true;
+            this.tabInput.HorizontalScrollbarHighlightOnWheel = false;
+            this.tabInput.HorizontalScrollbarSize = 10;
+            this.tabInput.Location = new System.Drawing.Point(4, 38);
+            this.tabInput.Name = "tabInput";
+            this.tabInput.Size = new System.Drawing.Size(1085, 662);
+            this.tabInput.TabIndex = 5;
+            this.tabInput.Text = "Редактор";
+            this.tabInput.VerticalScrollbarBarColor = true;
+            this.tabInput.VerticalScrollbarHighlightOnWheel = false;
+            this.tabInput.VerticalScrollbarSize = 10;
+            // 
+            // metroButtonLine
+            // 
+            this.metroButtonLine.Location = new System.Drawing.Point(157, 21);
+            this.metroButtonLine.Name = "metroButtonLine";
+            this.metroButtonLine.Size = new System.Drawing.Size(134, 39);
+            this.metroButtonLine.TabIndex = 4;
+            this.metroButtonLine.Text = "Линия";
+            this.metroButtonLine.UseSelectable = true;
+            this.metroButtonLine.Click += new System.EventHandler(this.metroButtonLine_Click);
+            // 
+            // metroButtonRect
+            // 
+            this.metroButtonRect.Location = new System.Drawing.Point(0, 21);
+            this.metroButtonRect.Name = "metroButtonRect";
+            this.metroButtonRect.Size = new System.Drawing.Size(134, 39);
+            this.metroButtonRect.TabIndex = 3;
+            this.metroButtonRect.Text = "Прямоугольник";
+            this.metroButtonRect.UseSelectable = true;
+            this.metroButtonRect.Click += new System.EventHandler(this.metroButtonRect_Click);
+            // 
+            // openGLControlView
+            // 
+            this.openGLControlView.DrawFPS = false;
+            this.openGLControlView.Location = new System.Drawing.Point(-1, 77);
+            this.openGLControlView.Name = "openGLControlView";
+            this.openGLControlView.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControlView.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControlView.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControlView.Size = new System.Drawing.Size(1082, 586);
+            this.openGLControlView.TabIndex = 2;
+            this.openGLControlView.OpenGLInitialized += new System.EventHandler(this.openGLControlView_OpenGLInitialized);
+            this.openGLControlView.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControlView_OpenGLDraw);
+            this.openGLControlView.Load += new System.EventHandler(this.openGLControlView_Load);
+            this.openGLControlView.Click += new System.EventHandler(this.openGLControlView_Click);
+            this.openGLControlView.DoubleClick += new System.EventHandler(this.openGLControlView_DoubleClick);
+            this.openGLControlView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.openGLControlView_MouseDown);
+            this.openGLControlView.MouseMove += new System.Windows.Forms.MouseEventHandler(this.openGLControlView_MouseMove);
+            this.openGLControlView.MouseUp += new System.Windows.Forms.MouseEventHandler(this.openGLControlView_MouseUp);
+            this.openGLControlView.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.openGLControlView_MouseWheel);
+            // 
+            // TabSettings
+            // 
+            this.TabSettings.HorizontalScrollbarBarColor = true;
+            this.TabSettings.HorizontalScrollbarHighlightOnWheel = false;
+            this.TabSettings.HorizontalScrollbarSize = 10;
+            this.TabSettings.Location = new System.Drawing.Point(4, 38);
+            this.TabSettings.Name = "TabSettings";
+            this.TabSettings.Padding = new System.Windows.Forms.Padding(25);
+            this.TabSettings.Size = new System.Drawing.Size(1085, 662);
+            this.TabSettings.TabIndex = 3;
+            this.TabSettings.Text = "Настройки";
+            this.TabSettings.VerticalScrollbarBarColor = true;
+            this.TabSettings.VerticalScrollbarHighlightOnWheel = false;
+            this.TabSettings.VerticalScrollbarSize = 10;
+            this.TabSettings.Visible = false;
+            // 
+            // tabHelp
+            // 
+            this.tabHelp.HorizontalScrollbarBarColor = true;
+            this.tabHelp.HorizontalScrollbarHighlightOnWheel = false;
+            this.tabHelp.HorizontalScrollbarSize = 10;
+            this.tabHelp.Location = new System.Drawing.Point(4, 38);
+            this.tabHelp.Name = "tabHelp";
+            this.tabHelp.Size = new System.Drawing.Size(1085, 662);
+            this.tabHelp.TabIndex = 4;
+            this.tabHelp.Text = "Справка";
+            this.tabHelp.VerticalScrollbarBarColor = true;
+            this.tabHelp.VerticalScrollbarHighlightOnWheel = false;
+            this.tabHelp.VerticalScrollbarSize = 10;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1133, 784);
+            this.Controls.Add(this.metroTabControl1);
+            this.Name = "MainForm";
+            this.Style = MetroFramework.MetroColorStyle.Silver;
+            this.Text = "Графический редактор";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroTabControl1.ResumeLayout(false);
+            this.TabFile.ResumeLayout(false);
+            this.TabFile.PerformLayout();
+            this.tabInput.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControlView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage TabFile;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroButton metroButtonLoadFile;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTabPage tabInput;
+        private MetroFramework.Controls.MetroTabPage TabSettings;
+        private MetroFramework.Controls.MetroTabPage tabHelp;
+        private MetroFramework.Controls.MetroButton metroButtonLine;
+        private MetroFramework.Controls.MetroButton metroButtonRect;
+        private SharpGL.OpenGLControl openGLControlView;
+    }
+}
+
---
+++ PaintApp/MainForm.cs
@@ -0,0 +1,228 @@
+﻿using MetroFramework.Forms;
+using SharpGL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintApp
+{
+    public partial class MainForm : MetroForm
+    {
+        enum Figures { Line, Quadrangle };
+
+        float firstPointX = 0;
+        float firstPointY = 0;
+
+        float lastPointX = 0;
+        float lastPointY = 0;
+
+        bool isMouseDown = false;
+        bool isMouseUp = false;
+
+        bool isChangedOpenGLView = true;
+        bool isLoadOpenGLView = false;
+
+        float zoomOpenGLView = 1.0f;
+
+        Figures selectedFigure = Figures.Line;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+        private void preGL2D(OpenGL gl, int width, int height)
+        {
+            gl.DrawBuffer(OpenGL.GL_FRONT);
+            gl.Viewport(0, 0, width, height);
+
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+
+            gl.LoadIdentity();
+
+            gl.Ortho(0, width, 0, height, -1, 1);
+
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+
+            gl.LoadIdentity();
+            
+            gl.ShadeModel(OpenGL.GL_SMOOTH);               // Enable Smooth Shading
+            gl.PixelStore(OpenGL.GL_UNPACK_ALIGNMENT, 4);  //4-byte pixel alignment
+            gl.Hint(OpenGL.GL_PERSPECTIVE_CORRECTION_HINT, OpenGL.GL_NICEST);
+
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+            gl.Enable(OpenGL.GL_LIGHTING);
+            gl.Enable(OpenGL.GL_TEXTURE_2D);
+            gl.Disable(OpenGL.GL_CULL_FACE);
+            gl.Enable(OpenGL.GL_BLEND);
+
+            gl.BlendFunc(OpenGL.GL_SRC_ALPHA, OpenGL.GL_ONE_MINUS_SRC_ALPHA);
+
+            gl.Enable(OpenGL.GL_LINE_SMOOTH);
+
+            gl.Hint(OpenGL.GL_LINE_SMOOTH_HINT, OpenGL.GL_NICEST);
+
+            gl.ColorMaterial(OpenGL.GL_FRONT_AND_BACK, OpenGL.GL_AMBIENT_AND_DIFFUSE);
+            gl.Enable(OpenGL.GL_COLOR_MATERIAL);
+
+            gl.ClearColor(1, 1, 1, 1);
+            gl.ClearStencil(0);
+            gl.ClearDepth(1.0);
+
+            gl.DepthFunc(OpenGL.GL_LEQUAL);
+
+            gl.Clear(OpenGL.GL_DEPTH_BUFFER_BIT | OpenGL.GL_COLOR_BUFFER_BIT);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openGLControlView_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openGLControlView_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openGLControlView_MouseDown(object sender, MouseEventArgs e)
+        {
+            firstPointX = e.X;
+            firstPointY = e.Y;
+
+            isMouseDown = true;
+            isMouseUp = false;
+            isChangedOpenGLView = true;
+        }
+
+        private void openGLControlView_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isLoadOpenGLView) return;
+            if (!isLoadOpenGLView) return;
+
+            if (isMouseDown)
+            {
+                lastPointX = e.X;
+                lastPointY = e.Y;
+
+                isChangedOpenGLView = true;
+            }
+        }
+
+
+        private void openGLControlView_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (!isLoadOpenGLView) return;
+
+            if (e.Delta < 0)
+            {
+                zoomOpenGLView *= 1.05f;
+            }
+            else
+            {
+                zoomOpenGLView *= 0.95f;
+            }
+            isChangedOpenGLView = true;
+        }
+
+        private void metroButtonRect_Click(object sender, EventArgs e)
+        {
+            selectedFigure = Figures.Quadrangle;
+        }
+
+        private void metroButtonLine_Click(object sender, EventArgs e)
+        {
+            selectedFigure = Figures.Line;
+        }
+
+        public void DrawQuadrangle(OpenGL gl, float firstPointX, float firstPointY, float lastPointX, float lastPointY)
+        {
+            gl.Begin(OpenGL.GL_QUADS);
+
+            gl.Color(0, 0, 0);
+            gl.Vertex(firstPointX, firstPointY);
+
+            gl.Color(0, 0, 0);
+            gl.Vertex(firstPointX, lastPointY);
+            
+            gl.Color(0, 0, 0);
+            gl.Vertex(lastPointX, lastPointY);
+            
+            gl.Color(0, 0, 0);
+            gl.Vertex(lastPointX, firstPointY);
+            gl.End();
+        }
+
+        public void DrawLine(OpenGL gl, float firstPointX, float firstPointY, float lastPointX, float lastPointY)
+        {
+            gl.Begin(OpenGL.GL_LINES);
+
+            gl.Color(0, 0, 0);
+            gl.Vertex(firstPointX, firstPointY);
+            
+            gl.Color(0, 0, 0);
+            gl.Vertex(lastPointX, lastPointY);
+            gl.End();
+        }
+
+        private void openGLControlView_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            if (!isLoadOpenGLView) return;
+            if (isChangedOpenGLView)
+            {
+                OpenGL gl = openGLControlView.OpenGL;
+                int contextWidth = openGLControlView.Width;
+                int contextHeight = openGLControlView.Height;
+                preGL2D(gl, openGLControlView.Width, openGLControlView.Height);
+
+                switch (selectedFigure)
+                {
+                    case Figures.Quadrangle:
+                        DrawQuadrangle(gl,
+                            firstPointX,
+                            openGLControlView.Height - firstPointY,
+                            lastPointX,
+                            openGLControlView.Height - lastPointY);
+                        break;
+                    case Figures.Line:
+                        DrawLine(gl,
+                            firstPointX,
+                            openGLControlView.Height - firstPointY,
+                            lastPointX,
+                            openGLControlView.Height - lastPointY);
+                        break;
+                }
+
+                gl.Flush();
+                isChangedOpenGLView = false;
+            }
+        }
+
+        
+
+        private void openGLControlView_OpenGLInitialized(object sender, EventArgs e)
+        {
+            isLoadOpenGLView = true;
+        }
+
+        private void openGLControlView_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+            isMouseUp = true;
+        }
+
+        private void openGLControlView_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
